05c500c808556fdc6f0c7b6a7ae2d2a6de33100c
Stefan Binder <binder_stefan@outlook.com>
2023-09-07 15:57:57 +0200

    Remove __ibis_sql folders from demo_project

--- demo_project/jaffle_shop/models/__ibis_sql/stores_filtered.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  t0.store_id,
-  t0.store_name
-FROM {{ ref('stg_stores') }} AS t0
-WHERE
-  t0.store_id = CAST(1 AS TINYINT)
--- demo_project/jaffle_shop/models/staging/__ibis_sql/stg_stores.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  CAST(t0.store_id AS BIGINT) AS store_id,
-  t0.store_name
-FROM {{ source('sources_db', 'stores') }} AS t0
